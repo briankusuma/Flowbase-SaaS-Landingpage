259c47025607e004ab5a58d7a3d9df033149b780
briankusuma <kusuma.brian@gmail.com>
2021-06-18 22:06:44 +0700

    navigation page and smoth scrolling

--- Flowbase-SaaS Landingpage/app.js
+++ Flowbase-SaaS Landingpage/app.js
@@ -1,2 +1,24 @@
 // gsap animation
 
+// let home = document.getElementsByClassName("home");
+// let feature = document.getElementsByClassName("feature");
+// let pricing = document.getElementsByClassName("pricing");
+// let blog = document.getElementsByClassName("blog");
+
+
+const links = document.querySelectorAll(".page-header ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
